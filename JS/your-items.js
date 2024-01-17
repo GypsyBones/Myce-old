70c4de23e600df21a042dc65d896667b4e0c1396
Gypsy Bones <gypsysabaj@gmail.com>
2024-01-17 08:50:50 -0600

    javascript uphaul

--- JS/your-items.js
+++ JS/your-items.js
@@ -1,64 +1,239 @@
-const addProductButton = document.getElementById('add-product');
-const deleteBtn = document.getElementById('delete-btn');
+const addProductButton = document.getElementById('addProductBtn');
 const productTable = document.getElementById('product-table');
 const productTableBody = document.querySelector('#product-table tbody');
+
 const inputProductName = document.getElementById('productName');
 const inputProductDesc = document.getElementById('productDescription');
 const inputPrice = document.getElementById('price');
 const inputQuantity = document.getElementById('quantity');
-const checkBox = document.getElementById('checkbox');
 
-const url_endpoint = "https://65a096c3600f49256fb0123d.mockapi.io/api/v1/Communities";
-
-
-addProductButton.addEventListener('click', (e) => {
-    e.preventDefault(); // literally just copy-pasted a row below and put in the value constants
-    const row = createHTMLElement(`
-    <tr>
-        <td>
-            <div class="img"></div>
-        </td>
-        <td>
-            <div class="Name">
-                <span><b>${inputProductName.value}</b></span><br>
-                <span><small>${inputProductDesc.value}</small></span>
-            </div>
-        </td>
-        <td>$${inputPrice.value}</td>
-        <td class="quantity">
-            <span><b>${inputQuantity.value}</b></span>
-        </td>
-        <td>$${inputPrice.value * inputQuantity.value}</td>
-        <td>
-            <button id="delete-btn" class="btn btn-dark text-danger">Delete</button>
-        </td>
-    </tr>`)
-    console.log(row)
-    productTableBody.append(row);
-    inputProductName.value = ''      // making sure that the inputs go back to nothing when adding the product
-    inputProductDesc.value = ''
-    inputPrice.value = ''
-    inputQuantity.value = ''
-})
-
-//tried to utilize the deleteBtn query selector but it would only delete the first row. I even put a console.log
-//      on there to see if it was even registering the click, but it wasn't?? 
-//deleteBtn.addEventListener('click', (e) => {
-//    let row = e.target.closest('tr');
-//    console.log(e.target)
-//    row.remove();
-//})
-
-//This is the click to delete function that was presented at class on Thursday.
-productTable.addEventListener('click', (e) => {
-    let row = e.target.closest('tr');
-    console.log(e.target)
-    row.remove();
-})
+const updateName = document.getElementById('updateName');
+const updateDesc = document.getElementById('updateDescription');
+const updatePrice = document.getElementById('updatePrice');
+const updateQty  = document.getElementById('updateQuantity');
 
 
-function createHTMLElement(innerHTML) {
-    const template = document.createElement('template');
-    template.innerHTML = innerHTML;
-    return template.content.firstElementChild;
+const endpoint_Communities = "https://65a096c3600f49256fb0123d.mockapi.io/api/v1/Communities";
+const endpoint_Users = "https://65a096c3600f49256fb0123d.mockapi.io/api/v1/Users";
+const endpoint_Posts = "https://65a096c3600f49256fb0123d.mockapi.io/api/v1/posts";
+const endpoint_Products = "https://65a096c3600f49256fb0123d.mockapi.io/api/v1/products";
+const endpoint_Events = "https://65a096c3600f49256fb0123d.mockapi.io/api/v1/Events";
+
+
+class Products {
+    constructor(title, description, price, qty) {
+        this.name = title;
+        this.description = description;
+        this.price = price;
+        this.qty = qty;
+    }
 }
+
+class ProductService {
+    static getAllProducts() {
+        return $.get(endpoint_Products);
+    }
+
+    static getProduct(id) {
+        return $.get(endpoint_Products + `/${id}`);
+    }
+
+    static createProduct(product) {
+        return $.post(endpoint_Products, product);
+    }
+
+    static updateProduct(product) {
+        return $.ajax({
+            url: endpoint_Products + `/${product.id}`,
+            dataType: 'json',
+            data: JSON.stringify(product),
+            contentType: 'application/json',
+            type: 'PUT',
+        });
+    }
+
+    static deleteProduct(id) {
+        console.log(id);
+        $.ajax(`${endpoint_Products}` + `/${id}`, {
+            type: "DELETE",
+        });
+    }
+}
+
+
+class DOMManager {
+    static products;
+
+    static getAllProducts() {
+        ProductService.getAllProducts().then(products => this.render(products));
+    }
+
+    static render(products) {
+        this.products = products;
+        $('#productTableBody').empty();
+        $.get(endpoint_Products).then(data => {
+            data.map(products => {
+                $('#productTableBody').prepend(
+                    $(`
+                    <tr>
+                        <td>
+                            <div class="id">
+                                <span><b>${products.id}<b><span>
+                            </div>
+                        </td>
+                        <td>
+                            <div class="Name">
+                                <span><b>${products.name}</b></span><br>
+                                <span><small>${products.description}</small></span>
+                            </div>
+                        </td>
+                        <td>$${products.price}</td>
+                        <td class="quantity">
+                            <span><b>${products.qty}</b></span>
+                        </td>
+                        <td>$${products.price * products.qty}</td>
+                        <td>
+                            <button class="btn btn-dark text-danger" onclick="DOMManager.deleteProduct(${products.id})">Delete</button>
+                        </td>
+                        <td>
+                            <button class="btn btn-dark text-primary" data-bs-toggle="collapse" data-bs-target="#collapseUpdate-${products.id}" type="button" aria-expanded="false" aria-controls="collapseUpdate-${products.id}">Edit</button>
+                        </td>
+                    </tr>
+                    <tr class="collapse" id="collapseUpdate-${products.id}">
+                        <form>
+                            <td>
+                                <div class="id">
+                                    <span><b>${products.id}<b><span>
+                                </div>
+                            </td>
+                            <td>
+                                <label for="updateName-${products.id}" class="form-label">Product Name</label>
+                                <input type="text" style="width: 250px" class="form-control col" id="updateName-${products.id}" placeholder="Words">
+                            </td>
+                            <td>
+                                <label for="updatePrice-${products.id}" class="form-label">Price</label>
+                                <input type="number" style="width: 100px" step="0.01" class="form-control col" id="updatePrice-${products.id}" placeholder="$0.00">
+                            </td>
+                            <td>
+                                <label for="updateQuantity-${products.id}" class="form-label">Quantity</label>
+                                <input type="number" style="width: 65px" class="form-control col" id="updateQuantity-${products.id}" placeholder="42">
+                            </td>
+                            <td>
+                                <label for="updateDescription-${products.id}" class="form-label col">Product Description</label>
+                                <input type="text" class="form-control" colspan="2" id="updateDescription-${products.id}" placeholder="Describe your Product">
+                            </td>
+                            <td>
+                                &nbsp;
+                            </td>
+                            <td>
+                                <button class="btn btn-dark text-primary" id="" onclick="DOMManager.updateProduct('${products.id}})">Done</button>
+                            </td>
+                        </form>    
+                    </tr>
+                    `)
+                )
+            })
+        })
+    }
+
+    static deleteProduct(id) {
+        console.log(`this is the id: ${id} in the deleted product`)
+        ProductService.deleteProduct(id).then(() => {
+            return ProductService.getAllProducts();
+        }).then((products) => this.render(products));
+    }
+
+    static createProduct() {
+        console.log(`createProduct ran`)
+        let newProduct = new Products(inputProductName.val(), Intl.NumberFormat().format(inputProductDesc.val()), inputPrice.val(), inputQuantity.val())
+        ProductService.updateProduct(newProduct) 
+        .then(() => {
+            return ProductService.getAllProducts;
+        })
+        .then((products) => this.render(products));
+    }
+
+    static updateProduct(id) {
+        ProductService.updateProduct(id)
+        .then(() => {
+            return ProductService.getAllProducts();
+        })
+        .then((products) => this.render(products));
+    }
+}
+
+
+$('#addProductBtn').on('click', function(){
+    DOMManager.createProduct(inputProductName).val();
+    inputProductName.val('');
+});
+
+
+DOMManager.getAllProducts();
+
+
+
+
+
+/*
+
+async function getProducts() {
+    fetch(endpoint_Products)
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch((error) => console.error('Error:', error))
+}
+
+
+
+$.get(endpoint_Products).then(data => {
+    data.map(products => {
+        $('tbody').append(
+            $(`
+            <tr>
+                <td>
+                    <div class="id">
+                        <span><b>${products.id}<b><span>
+                    </div>
+                </td>
+                <td>
+                    <div class="Name">
+                        <span><b>${products.name}</b></span><br>
+                        <span><small>${products.description}</small></span>
+                    </div>
+                </td>
+                <td>$${products.price}</td>
+                <td class="quantity">
+                    <span><b>${products.qty}</b></span>
+                </td>
+                <td>$${products.price * products.qty}</td>
+                <td>
+                    <button class="btn btn-dark text-danger" onclick="deleteProduct(${products.id})">Delete</button>
+                </td>
+            </tr>
+            `)
+        )
+    })
+})
+
+function deleteProduct(id) {
+    $.ajax(`${endpoint_Products}` + `/${id}`, {
+        type: "DELETE",
+    })
+    console.log(id)
+    location.reload()
+}
+
+static deleteProduct(id) {
+    $.ajax(`${endpoint_Products}` + `/${id}`, {
+        type: "DELETE",
+    })
+    console.log(id)
+    DOMManager.render()
+
+
+//let productPrice = Intl.NumberFormat('en-US').format($('#price').val())
+
+*/
+
+
